1884c0d91e95ad9d3b76c789a473abd4f6ae24de
Administrator <Administrator@DELL>
2020-10-30 18:52:06 +0530

    Initial commit with project guide.

---
+++ SpringBootWithDevTools/src/main/java/com/javaLive/main/SpringBootWithDevToolsApplication.java
@@ -0,0 +1,13 @@
+package com.javaLive.main;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootWithDevToolsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootWithDevToolsApplication.class, args);
+	}
+
+}
